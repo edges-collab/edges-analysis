94e4f305e8b9c5441f504e63a991d44d48b2d9c9
raul <raul.monsalve@mcgill.ca>
2018-12-05 17:44:19 +0900

    Getdist functions

---
+++ getdist_plots.py
@@ -0,0 +1,86 @@
+
+
+import sys
+sys.path.insert(0, '/home/raul/GetDist-0.2.8.4.2')
+
+import numpy as np
+import matplotlib.pyplot as plt
+#import getdist as getdist
+from getdist import MCSamples, plots
+
+
+
+
+
+
+
+def load_samples(input_textfile, label_names=[]):
+	
+	
+	# Loading data
+	d = np.genfromtxt(input_textfile)
+
+	ww = d[:,0]    # Weights
+	ll = d[:,1]/2  # Minus Log Likelihood
+	ss = d[:,2::]  # Parameter samples
+	
+	
+	
+	# Parameter names and labels
+	Npar = len(ss[0,:])
+	names = ['x'+str(i+1) for i in range(Npar)]
+
+	if len(label_names)==0:
+		labels = [r'a_'+str(i) for i in range(Npar)]
+	else:
+		labels = label_names
+
+	
+	
+	# Convert samples into GETDIST format 
+	getdist_samples = MCSamples(samples=ss, weights=ww, loglikes=ll, names=names, labels=labels, label=r'only')
+
+
+	# Best fit and covariance matrix
+	IX = np.argmin(d[:,1])    # the maximum likelihood point
+	best_fit = d[IX, 2::]
+	
+	covariance_matrix = getdist_samples.cov()
+	
+	
+
+
+
+	return getdist_samples, best_fit, covariance_matrix
+
+
+
+
+
+
+
+
+
+
+
+
+def triangle_plot(getdist_samples, output_pdf, legend_FS=10, label_FS=10, axes_FS=10):
+
+	g = plots.getSubplotPlotter(subplot_size=1.5)
+
+	g.settings.legend_fontsize = legend_FS
+	g.settings.lab_fontsize    = label_FS
+	g.settings.axes_fontsize   = axes_FS
+
+	g.triangle_plot([getdist_samples], filled=True) #, param_limits={'x1':[1498, 1502], 'x2':[-2.504, -2.496]}, filled=True, legend_loc='upper right')
+	g.export(output_pdf)
+	plt.close()
+	plt.close()
+
+
+	return 0
+
+
+
+
+
--- mid_band_polychord.py
+++ mid_band_polychord.py
@@ -48,11 +48,9 @@ pl[2,1] =  1e3
 pl[3,0] = -1e3
 pl[3,1] =  1e3
 
-pl[4,0] = -0.2
-pl[4,1] =  0.0
+pl[4,0] = -1e3
+pl[4,1] =  1e3
 
-pl[5,0] = 0
-pl[5,1] = 4000
 
 
 
@@ -87,7 +85,7 @@ def full_model(theta):
 	
 	
 	# Foreground model
-	model_fg = dm.foreground_model(foreground_model, theta, v, v0, ion_abs_coeff='free', ion_emi_coeff='free')
+	model_fg = dm.foreground_model(foreground_model, theta, v, v0, ion_abs_coeff=0, ion_emi_coeff=0)
 
 
 	model = model_21 + model_fg
@@ -235,7 +233,7 @@ def run():
 
 
 #
-t, sigma, inv_sigma, det_sigma = simulated_data([1500, -2.5, 0.1, -4, -0.05, 800], 0.02)  # power law
+t, sigma, inv_sigma, det_sigma = simulated_data([1500, -2.5, 0.1, -4, -0.05], 0.02)  # power law
 #plt.plot(v, t)
 #plt.show()
 
