9e883481af036b60e4b482344e9faaf8ecad7e1b
Steven Murray <steven.murray@curtin.edu.au>
2020-02-19 06:29:26 -0700

    Adjust flake8 pre-commit repo

--- src/edges_analysis/analysis/io.py
+++ src/edges_analysis/analysis/io.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from edges_cal import modelling as mdl
 from . import tools
 
-edges_folder = ''  # TODO: remove this
+edges_folder = ""  # TODO: remove this
 
 
 def auxiliary_data(weather_file, thermlog_file, band, year, day):
@@ -21,7 +21,7 @@ def auxiliary_data(weather_file, thermlog_file, band, year, day):
     # Desktop/
 
     # Gather data from 'weather.txt' file
-    f1 = open(weather_file, 'r')
+    f1 = open(weather_file, "r")
     lines_all_1 = f1.readlines()
 
     array1 = np.zeros((0, 4))
@@ -47,7 +47,7 @@ def auxiliary_data(weather_file, thermlog_file, band, year, day):
             # print(line1[0:17] + ' ' + line1[59:65] + ' ' + line1[88:93] + ' ' + line1[113:119])
 
             date_time = line1[0:17]
-            ttt = date_time.split(':')
+            ttt = date_time.split(":")
             seconds = 3600 * int(ttt[2]) + 60 * int(ttt[3]) + int(ttt[4])
 
             try:
@@ -69,7 +69,7 @@ def auxiliary_data(weather_file, thermlog_file, band, year, day):
             array1_temp2 = array1_temp1.reshape((1, -1))
             array1 = np.append(array1, array1_temp2, axis=0)
 
-            print('weather time: ' + date_time)
+            print("weather time: " + date_time)
 
         i1 = i1 + 1
         print(i1)
@@ -79,42 +79,42 @@ def auxiliary_data(weather_file, thermlog_file, band, year, day):
             day_of_year_1 = int(line1[5:8])
 
     # gather data from 'thermlog.txt' file
-    f2 = open(thermlog_file, 'r')
+    f2 = open(thermlog_file, "r")
     lines_all_2 = f2.readlines()
 
     array2 = np.zeros((0, 2))
 
-    if (band == 'high_band') and (year == 2015):
+    if (band == "high_band") and (year == 2015):
         i2 = 24000  # ~ day 108
 
-    elif (band == 'high_band') and (year == 2016):
+    elif (band == "high_band") and (year == 2016):
         i2 = 58702  # beginning of year 2016
 
-    elif (band == 'low_band') and (year == 2015):
+    elif (band == "low_band") and (year == 2015):
         i2 = 0
 
-    elif (band == 'low_band') and (year == 2016):
+    elif (band == "low_band") and (year == 2016):
         i2 = 14920  # beginning of year 2016
 
-    elif (band == 'low_band') and (year == 2017):
+    elif (band == "low_band") and (year == 2017):
         i2 = 59352  # beginning of year 2017
 
-    elif (band == 'low_band2') and (year == 2017) and (day < 332):
+    elif (band == "low_band2") and (year == 2017) and (day < 332):
         return array1, np.array([0])
 
-    elif band == 'low_band2' and year == 2017:
+    elif band == "low_band2" and year == 2017:
         i2 = 0
 
-    elif (band == 'low_band2') and (year == 2018):
+    elif (band == "low_band2") and (year == 2018):
         i2 = 4768
 
-    elif (band == 'low_band3') and (year == 2018):
+    elif (band == "low_band3") and (year == 2018):
         i2 = 0  # 33826
 
-    elif (band == 'mid_band') and (year == 2018) and (day <= 171):
+    elif (band == "mid_band") and (year == 2018) and (day <= 171):
         i2 = 5624  # beginning of year 2018, file "thermlog_mid.txt"
 
-    elif (band == 'mid_band') and (year == 2018) and (day >= 172):
+    elif (band == "mid_band") and (year == 2018) and (day >= 172):
         i2 = 16154
 
     line2 = lines_all_2[i2]
@@ -127,7 +127,7 @@ def auxiliary_data(weather_file, thermlog_file, band, year, day):
             # print(line2[0:17] + ' ' + line2[48:53])
 
             date_time = line2[0:17]
-            ttt = date_time.split(':')
+            ttt = date_time.split(":")
             seconds = 3600 * int(ttt[2]) + 60 * int(ttt[3]) + int(ttt[4])
 
             try:
@@ -139,7 +139,7 @@ def auxiliary_data(weather_file, thermlog_file, band, year, day):
             array2_temp2 = array2_temp1.reshape((1, -1))
             array2 = np.append(array2, array2_temp2, axis=0)
 
-            print('receiver temperature time: ' + date_time)
+            print("receiver temperature time: " + date_time)
 
         i2 = i2 + 1
         if i2 != 26348:
@@ -150,86 +150,96 @@ def auxiliary_data(weather_file, thermlog_file, band, year, day):
     return array1, array2
 
 
-def level2read(path_file, print_key='no'):
+def level2read(path_file, print_key="no"):
     # path_file = home_folder + '/EDGES/spectra/level2/mid_band/file.hdf5'
 
     # Show keys (array names inside HDF5 file)
-    with h5py.File(path_file, 'r') as hf:
-        if print_key == 'yes':
+    with h5py.File(path_file, "r") as hf:
+        if print_key == "yes":
             print([key for key in hf.keys()])
 
-        hf_freq = hf.get('frequency')
+        hf_freq = hf.get("frequency")
         freq = np.array(hf_freq)
 
-        hf_Ta = hf.get('antenna_temperature')
+        hf_Ta = hf.get("antenna_temperature")
         Ta = np.array(hf_Ta)
 
-        hf_meta = hf.get('metadata')
+        hf_meta = hf.get("metadata")
         meta = np.array(hf_meta)
 
-        hf_weights = hf.get('weights')
+        hf_weights = hf.get("weights")
         weights = np.array(hf_weights)
 
     return freq, Ta, meta, weights
 
 
-def level3read(path_file, print_key='no'):
+def level3read(path_file, print_key="no"):
     # path_file = home_folder + '/EDGES/spectra/level3/mid_band/nominal_60_160MHz/file.hdf5'
 
     # Show keys (array names inside HDF5 file)
-    with h5py.File(path_file, 'r') as hf:
-        if print_key == 'yes':
+    with h5py.File(path_file, "r") as hf:
+        if print_key == "yes":
             print([key for key in hf.keys()])
 
-        hfX = hf.get('frequency')
+        hfX = hf.get("frequency")
         f = np.array(hfX)
 
-        hfX = hf.get('antenna_temperature')
+        hfX = hf.get("antenna_temperature")
         t = np.array(hfX)
 
-        hfX = hf.get('parameters')
+        hfX = hf.get("parameters")
         p = np.array(hfX)
 
-        hfX = hf.get('residuals')
+        hfX = hf.get("residuals")
         r = np.array(hfX)
 
-        hfX = hf.get('weights')
+        hfX = hf.get("weights")
         w = np.array(hfX)
 
-        hfX = hf.get('rms')
+        hfX = hf.get("rms")
         rms = np.array(hfX)
 
-        hfX = hf.get('total_power')
+        hfX = hf.get("total_power")
         tp = np.array(hfX)
 
-        hfX = hf.get('metadata')
+        hfX = hf.get("metadata")
         m = np.array(hfX)
 
     return f, t, p, r, w, rms, tp, m
 
 
-def level3_single_file_test(path_file, GHA_1, GHA_2, FLOW, FHIGH, plot_residuals_yes_no, model_type,
-                            Nfg, save_yes_no, save_spectrum_name):
+def level3_single_file_test(
+    path_file,
+    GHA_1,
+    GHA_2,
+    FLOW,
+    FHIGH,
+    plot_residuals_yes_no,
+    model_type,
+    Nfg,
+    save_yes_no,
+    save_spectrum_name,
+):
     f, t, p, r, w, rms, tp, m = level3read(path_file)
 
     gha = m[:, 4]
     gha[gha < 0] = gha[gha < 0] + 24
 
     if GHA_2 > GHA_1:
-        avr, avw = tools.spectral_averaging(r[(gha >= GHA_1) & (gha <= GHA_2), :],
-                                         w[(gha >= GHA_1) & (gha <= GHA_2), :])
+        avr, avw = tools.spectral_averaging(
+            r[(gha >= GHA_1) & (gha <= GHA_2), :], w[(gha >= GHA_1) & (gha <= GHA_2), :]
+        )
         avp = np.mean(p[(gha >= GHA_1) & (gha <= GHA_2), :], axis=0)
 
     if GHA_2 < GHA_1:
-        avr, avw = tools.spectral_averaging(r[(gha >= GHA_1) | (gha <= GHA_2), :],
-                                         w[(gha >= GHA_1) | (gha <= GHA_2), :])
+        avr, avw = tools.spectral_averaging(
+            r[(gha >= GHA_1) | (gha <= GHA_2), :], w[(gha >= GHA_1) | (gha <= GHA_2), :]
+        )
         avp = np.mean(p[(gha >= GHA_1) | (gha <= GHA_2), :], axis=0)
 
-    fb, rb, wb, sb = tools.spectral_binning_number_of_samples(
-        f, avr, avw, nsamples=128
-    )
+    fb, rb, wb, sb = tools.spectral_binning_number_of_samples(f, avr, avw, nsamples=128)
 
-    mb = mdl.model_evaluate('LINLOG', avp, fb / 200)
+    mb = mdl.model_evaluate("LINLOG", avp, fb / 200)
 
     tb = mb + rb
 
@@ -238,11 +248,13 @@ def level3_single_file_test(path_file, GHA_1, GHA_2, FLOW, FHIGH, plot_residuals
     ww = wb[(fb >= FLOW) & (fb <= FHIGH)]
     ss = sb[(fb >= FLOW) & (fb <= FHIGH)]
 
-    if plot_residuals_yes_no == 'yes':
-        if model_type == 'LINLOG':
-            pp = mdl.fit_polynomial_fourier('LINLOG', ff, tt, Nfg, Weights=1 / (ss ** 2))
+    if plot_residuals_yes_no == "yes":
+        if model_type == "LINLOG":
+            pp = mdl.fit_polynomial_fourier(
+                "LINLOG", ff, tt, Nfg, Weights=1 / (ss ** 2)
+            )
             model = pp[1]
-        elif model_type == 'LOGLOG':
+        elif model_type == "LOGLOG":
             pp = np.polyfit(np.log(ff), np.log(tt), Nfg - 1)
             log_model = np.polyval(pp, np.log(ff))
             model = np.exp(log_model)
@@ -252,36 +264,46 @@ def level3_single_file_test(path_file, GHA_1, GHA_2, FLOW, FHIGH, plot_residuals
         plt.ylim([-1, 1])
         plt.xlim([60, 120])
 
-    if save_yes_no == 'yes':
+    if save_yes_no == "yes":
         outT = np.array([ff, tt, ww, ss])
         out = outT.T
 
-        save_path = edges_folder + 'mid_band/spectra/level5/one_day_tests/'
+        save_path = edges_folder + "mid_band/spectra/level5/one_day_tests/"
         np.savetxt(save_path + save_spectrum_name, out)
 
     return ff, tt, ww, ss
 
 
-def data_selection(m, GHA_or_LST='GHA', TIME_1=0, TIME_2=24, sun_el_max=90, moon_el_max=90,
-                   amb_hum_max=200, min_receiver_temp=0, max_receiver_temp=100):
+def data_selection(
+    m,
+    GHA_or_LST="GHA",
+    TIME_1=0,
+    TIME_2=24,
+    sun_el_max=90,
+    moon_el_max=90,
+    amb_hum_max=200,
+    min_receiver_temp=0,
+    max_receiver_temp=100,
+):
     # Master index
     index = np.arange(len(m[:, 0]))
 
-    if GHA_or_LST == 'GHA':
+    if GHA_or_LST == "GHA":
         GHA = m[:, 4]
         GHA[GHA < 0] = GHA[GHA < 0] + 24
 
         index_TIME_1 = index[GHA >= TIME_1]
         index_TIME_2 = index[GHA < TIME_2]
-    elif GHA_or_LST == 'LST':
+    elif GHA_or_LST == "LST":
         index_TIME_1 = index[m[:, 3] >= TIME_1]
         index_TIME_2 = index[m[:, 3] < TIME_2]
     # Sun elevation, Moon elevation, ambient humidity, and receiver temperature
     index_SUN = index[m[:, 6] <= sun_el_max]
     index_MOON = index[m[:, 8] <= moon_el_max]
     index_HUM = index[m[:, 10] <= amb_hum_max]
-    index_Trec = index[(m[:, 11] >= min_receiver_temp) & (m[:,
-                                                          11] <= max_receiver_temp)]  #
+    index_Trec = index[
+        (m[:, 11] >= min_receiver_temp) & (m[:, 11] <= max_receiver_temp)
+    ]  #
     # Combined index
     if TIME_1 < TIME_2:
         index1 = np.intersect1d(index_TIME_1, index_TIME_2)
@@ -296,49 +318,49 @@ def data_selection(m, GHA_or_LST='GHA', TIME_1=0, TIME_2=24, sun_el_max=90, moon
 
 
 def level4read(path_file):
-    with h5py.File(path_file, 'r') as hf:
-        hfX = hf.get('frequency')
+    with h5py.File(path_file, "r") as hf:
+        hfX = hf.get("frequency")
         f = np.array(hfX)
 
-        hfX = hf.get('parameters')
+        hfX = hf.get("parameters")
         p_all = np.array(hfX)
 
-        hfX = hf.get('residuals')
+        hfX = hf.get("residuals")
         r_all = np.array(hfX)
 
-        hfX = hf.get('weights')
+        hfX = hf.get("weights")
         w_all = np.array(hfX)
 
-        hfX = hf.get('index')
+        hfX = hf.get("index")
         index = np.array(hfX)
 
-        hfX = hf.get('gha_edges')
+        hfX = hf.get("gha_edges")
         gha = np.array(hfX)
 
-        hfX = hf.get('year_day')
+        hfX = hf.get("year_day")
         yd = np.array(hfX)
 
     return f, p_all, r_all, w_all, index, gha, yd
 
 
 def level4_binned_read(path_file):
-    with h5py.File(path_file, 'r') as hf:
-        hfX = hf.get('frequency')
+    with h5py.File(path_file, "r") as hf:
+        hfX = hf.get("frequency")
         fb = np.array(hfX)
 
-        hfX = hf.get('residuals')
+        hfX = hf.get("residuals")
         rb = np.array(hfX)
 
-        hfX = hf.get('weights')
+        hfX = hf.get("weights")
         wb = np.array(hfX)
 
-        hfX = hf.get('stddev')
+        hfX = hf.get("stddev")
         sb = np.array(hfX)
 
-        hfX = hf.get('gha_edges')
+        hfX = hf.get("gha_edges")
         gha = np.array(hfX)
 
-        hfX = hf.get('year_day')
+        hfX = hf.get("year_day")
         yd = np.array(hfX)
 
     return fb, rb, wb, sb, gha, yd
@@ -346,42 +368,61 @@ def level4_binned_read(path_file):
 
 def level4_save_averaged_spectra(case, GHA_case, first_day, last_day):
     if case == 2:
-        header_text = 'f [MHz], t_ant (GHA=0-23) [K], std (GHA=0-23) [K], Nsamples (GHA=0-23)'
-        file_path = edges_folder + \
-                    'mid_band/spectra/level4/calibration_2019_10_no_ground_loss_no_beam_corrections' \
-                    '/binned_averages/'
-        file_name = 'GHA_every_1hr.txt'
+        header_text = (
+            "f [MHz], t_ant (GHA=0-23) [K], std (GHA=0-23) [K], Nsamples (GHA=0-23)"
+        )
+        file_path = (
+            edges_folder
+            + "mid_band/spectra/level4/calibration_2019_10_no_ground_loss_no_beam_corrections"
+            "/binned_averages/"
+        )
+        file_name = "GHA_every_1hr.txt"
     elif case == 3:
-        header_text = 'f [MHz], t_ant (GHA=0-23) [K], std (GHA=0-23) [K], Nsamples (GHA=0-23)'
-        file_path = edges_folder + \
-                    'mid_band/spectra/level4/case_nominal_50' \
-                    '-150MHz_no_ground_loss_no_beam_corrections' \
-                    '/binned_averages/'
-        file_name = 'GHA_every_1hr.txt'
+        header_text = (
+            "f [MHz], t_ant (GHA=0-23) [K], std (GHA=0-23) [K], Nsamples (GHA=0-23)"
+        )
+        file_path = (
+            edges_folder + "mid_band/spectra/level4/case_nominal_50"
+            "-150MHz_no_ground_loss_no_beam_corrections"
+            "/binned_averages/"
+        )
+        file_name = "GHA_every_1hr.txt"
     elif case == 406:
-        header_text = 'f [MHz], t_ant (GHA=0-23) [K], std (GHA=0-23) [K], Nsamples (GHA=0-23)'
-        file_path = edges_folder + \
-                    'mid_band/spectra/level4/case_nominal_50-150MHz_LNA1_a2_h2_o2_s1_sim2' \
-                    '/binned_averages/'
-        file_name = 'GHA_every_1hr.txt'
+        header_text = (
+            "f [MHz], t_ant (GHA=0-23) [K], std (GHA=0-23) [K], Nsamples (GHA=0-23)"
+        )
+        file_path = (
+            edges_folder
+            + "mid_band/spectra/level4/case_nominal_50-150MHz_LNA1_a2_h2_o2_s1_sim2"
+            "/binned_averages/"
+        )
+        file_name = "GHA_every_1hr.txt"
     elif case == 5:
-        header_text = 'f [MHz], t_ant (GHA=0-23) [K], std (GHA=0-23) [K], Nsamples (GHA=0-23)'
-        file_path = edges_folder + \
-                    'mid_band/spectra/level4/case_nominal_14_14_terms_55' \
-                    '-150MHz_no_ground_loss_no_beam_corrections/binned_averages/'
-        file_name = 'GHA_every_1hr.txt'
+        header_text = (
+            "f [MHz], t_ant (GHA=0-23) [K], std (GHA=0-23) [K], Nsamples (GHA=0-23)"
+        )
+        file_path = (
+            edges_folder + "mid_band/spectra/level4/case_nominal_14_14_terms_55"
+            "-150MHz_no_ground_loss_no_beam_corrections/binned_averages/"
+        )
+        file_name = "GHA_every_1hr.txt"
     elif case == 501:
-        header_text = 'f [MHz], t_ant (GHA=0-23) [K], std (GHA=0-23) [K], Nsamples (GHA=0-23)'
-        file_path = edges_folder + \
-                    'mid_band/spectra/level4/case_nominal_50' \
-                    '-150MHz_LNA2_a2_h2_o2_s1_sim2_all_lc_yes_bc' \
-                    '/binned_averages/'
-        file_name = 'GHA_every_1hr.txt'
+        header_text = (
+            "f [MHz], t_ant (GHA=0-23) [K], std (GHA=0-23) [K], Nsamples (GHA=0-23)"
+        )
+        file_path = (
+            edges_folder + "mid_band/spectra/level4/case_nominal_50"
+            "-150MHz_LNA2_a2_h2_o2_s1_sim2_all_lc_yes_bc"
+            "/binned_averages/"
+        )
+        file_name = "GHA_every_1hr.txt"
     start = 0
 
     if GHA_case == 24:
         for i in range(24):
-            fb, tb, rb, wb, sb = tools.level4_integration(case, [i], first_day, last_day, 55, 150, 5)
+            fb, tb, rb, wb, sb = tools.level4_integration(
+                case, [i], first_day, last_day, 55, 150, 5
+            )
             if start == 0:
                 outT = np.zeros((1 + 3 * 24, len(fb)))
                 outT[0, :] = fb
@@ -398,36 +439,36 @@ def level4_save_averaged_spectra(case, GHA_case, first_day, last_day):
 
 
 def level4_foreground_fits_read(path_file):
-    with h5py.File(path_file, 'r') as hf:
-        hfX = hf.get('fref')
+    with h5py.File(path_file, "r") as hf:
+        hfX = hf.get("fref")
         fref = np.array(hfX)
 
-        hfX = hf.get('fit2')
+        hfX = hf.get("fit2")
         fit2 = np.array(hfX)
 
-        hfX = hf.get('fit3')
+        hfX = hf.get("fit3")
         fit3 = np.array(hfX)
 
-        hfX = hf.get('fit4')
+        hfX = hf.get("fit4")
         fit4 = np.array(hfX)
 
-        hfX = hf.get('fit5')
+        hfX = hf.get("fit5")
         fit5 = np.array(hfX)
 
     return fref, fit2, fit3, fit4, fit5
 
+
 def calibration_RMS_read(path_file):
 
-	with h5py.File(path_file,'r') as hf:
+    with h5py.File(path_file, "r") as hf:
 
-		hfX    = hf.get('RMS')
-		RMS    = np.array(hfX)
+        hfX = hf.get("RMS")
+        RMS = np.array(hfX)
 
-		hfX    = hf.get('index_cterms')
-		cterms = np.array(hfX)
+        hfX = hf.get("index_cterms")
+        cterms = np.array(hfX)
 
-		hfX    = hf.get('index_wterms')
-		wterms = np.array(hfX)
+        hfX = hf.get("index_wterms")
+        wterms = np.array(hfX)
 
-
-	return RMS, cterms, wterms
+    return RMS, cterms, wterms
