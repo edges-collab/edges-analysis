f7c76ec8fd27183df53770c47159dfc124ecc987
Steven Murray <steven.murray@curtin.edu.au>
2020-03-03 15:37:26 -0700

    Remove rest of script functions

--- old_functions.py
+++ old_functions.py
@@ -18,6 +18,507 @@ from edges_analysis.analysis.scripts import (
     models_antenna_s11_remove_delay,
     batch_level1_to_level2,
 )
+from edges_analysis.estimation.plots import triangle_plot, load_samples
+from edges_analysis.estimation import plots as eplt
+
+
+def plots_midband_paper(plot_number, s11_path="antenna_s11_2018_147_17_04_33.txt"):
+    # TODO: move to old
+    if plot_number == 1:
+        eplt.plot_receiver_calibration_params()
+    elif plot_number == 10:
+        eplt.plot_data_stats()
+    elif plot_number == 11:
+        eplt.plot_low_mid_comparison()
+    elif plot_number == 12:
+        eplt.plot_sky_model_comparison()
+    elif plot_number == 121:
+        eplt.plot_beam_gain()
+    elif plot_number == 13:
+        eplt.plot_sky_model()
+    elif plot_number == 14:
+        eplt.plot_absorption_model_comparison()
+    elif plot_number == 2:
+        eplt.plot_antenna_calibration_params(s11_path)
+    elif plot_number == 3:
+        eplt.plot_balun_loss2(s11_path)
+    elif plot_number == 4:
+        eplt.plot_antenna_beam()
+    elif plot_number == 5:
+        eplt.plot_beam_chromaticity_correction()
+    elif plot_number == 50:
+        eplt.plot_ground_loss()
+    elif plot_number == 500:
+        eplt.plot_calibration_parameters(s11_path)
+    elif plot_number == 501:
+        eplt.plot_balun_loss()
+    elif plot_number == 6:
+        eplt.beam_chromaticity_differences()
+    elif plot_number == 7:
+        eplt.plot_beam_power()
+    elif plot_number == 8:
+        eplt.plot_foreground_polychord_fit(
+            datafile=(
+                config["edges_folder"] + "mid_band/spectra/level5/case_nominal"
+                "/integrated_spectrum_case_nominal_days_186_219_58-120MHz.txt"
+            ),
+            fg_chain_root=(
+                config["edges_folder"]
+                + "mid_band/polychord/20190815/case_nominal/foreground_powerlog_5par/chain"
+            ),
+            full_chain_root=(
+                config["edges_folder"] + "mid_band/polychord/20190815/case_nominal"
+                "/foreground_powerlog_5par_signal_exp_4par/chain"
+            ),
+            f_low=58,
+            f_high=120,
+            save_path=config["edges_folder"] + "plots/20190815/",
+        )
+    elif plot_number == 9:
+        triangle_plot(
+            file_root=(
+                config["edges_folder"] + "mid_band/polychord/20190617/case2"
+                "/foreground_exp_signal_exp_4par_60_120MHz/chain"
+            ),
+            output_file=(
+                config["edges_folder"]
+                + "plots/20190617/triangle_plot_exp_exp_4par_60_120MHz.pdf"
+            ),
+        )
+
+
+def plots_midband_polychord(fig):
+    # TODO: move to old
+
+    if fig == 0:
+        folder = (
+            config["edges_folder"]
+            + "mid_band/polychord/20190508/case1_nominal/foreground_model_exp/"
+        )
+        getdist_samples, ww, ll, best_fit, covariance_matrix = load_samples(
+            folder + "chain.txt",
+            0,
+            label_names=[
+                r"T_{100}\;[{\rm K}]",
+                r"\beta",
+                r"\gamma",
+                r"\delta",
+                r"\epsilon",
+            ],
+        )
+        triangle_plot(
+            getdist_samples, folder + "result.pdf", legend_FS=10, label_FS=10, axes_FS=5
+        )
+
+        v, t, w, sigma, inv_sigma, det_sigma = dm.real_data(10, 60, 120)
+
+        model = dm.full_model(
+            best_fit,
+            v,
+            100,
+            model_type_signal="exp",
+            model_type_foreground="exp",
+            n_21=0,
+            n_fgpar=5,
+        )
+    elif fig == 1:
+        folder = (
+            config["edges_folder"]
+            + "mid_band/polychord/20190508/case1_nominal/foreground_model_linlog/"
+        )
+        getdist_samples, ww, ll, best_fit, covariance_matrix = load_samples(
+            folder + "chain.txt",
+            0,
+            label_names=[r"a_0", r"a_1", r"a_2", r"a_3", r"a_4"],
+        )
+        triangle_plot(
+            getdist_samples, folder + "result.pdf", legend_FS=10, label_FS=12, axes_FS=8
+        )
+
+        v, t, w, sigma, inv_sigma, det_sigma = dm.real_data(10, 60, 120)
+
+        model = dm.full_model(
+            best_fit,
+            v,
+            100,
+            model_type_signal="exp",
+            model_type_foreground="linlog",
+            n_21=0,
+            n_fgpar=5,
+        )
+    elif fig == 10:
+        # Data used:  60-120
+        folder = (
+            config["edges_folder"] + "mid_band/polychord/20190508/case1_nominal"
+            "/foreground_model_exp_signal_model_tanh_60_120MHz/"
+        )
+        getdist_samples, ww, ll, best_fit, covariance_matrix = load_samples(
+            folder + "chain.txt",
+            0,
+            label_names=[
+                r"A\;[{\rm K}]",
+                r"\nu_0\;[{\rm MHz}]",
+                r"w\;[{\rm MHz}]",
+                r"\tau_1",
+                r"\tau_2",
+                r"T_{100}\;[{\rm K}]",
+                r"\beta",
+                r"\gamma",
+                r"\delta",
+                r"\epsilon",
+            ],
+        )
+        triangle_plot(
+            getdist_samples, folder + "result.pdf", legend_FS=10, label_FS=13, axes_FS=7
+        )
+
+        v, t, w, sigma, inv_sigma, det_sigma = dm.real_data(10, 60, 120)
+
+        model = dm.full_model(
+            best_fit,
+            v,
+            100,
+            model_type_signal="tanh",
+            model_type_foreground="exp",
+            n_21=5,
+            n_fgpar=5,
+        )
+    elif fig == 11:
+        # Data used:  60-120, CASE 2, cterms7, wterms8
+        folder = (
+            config["edges_folder"] + "mid_band/polychord/20190508/case1_nominal"
+            "/foreground_model_exp_signal_model_tanh_60_120MHz_case2/"
+        )
+        getdist_samples, ww, ll, best_fit, covariance_matrix = load_samples(
+            folder + "chain.txt",
+            0,
+            label_names=[
+                r"A\;[{\rm K}]",
+                r"\nu_0\;[{\rm MHz}]",
+                r"w\;[{\rm MHz}]",
+                r"\tau_1",
+                r"\tau_2",
+                r"T_{100}\;[{\rm K}]",
+                r"\beta",
+                r"\gamma",
+                r"\delta",
+                r"\epsilon",
+            ],
+        )
+        triangle_plot(
+            getdist_samples, folder + "result.pdf", legend_FS=10, label_FS=13, axes_FS=7
+        )
+
+        v, t, w, sigma, inv_sigma, det_sigma = dm.real_data(2, 60, 120)
+
+        model = dm.full_model(
+            best_fit,
+            v,
+            100,
+            model_type_signal="tanh",
+            model_type_foreground="exp",
+            n_21=5,
+            n_fgpar=5,
+        )
+    elif fig == 12:
+        # Data used:  60-120
+        folder = (
+            config["edges_folder"] + "mid_band/polychord/20190508/case1_nominal"
+            "/foreground_model_exp_signal_model_exp_60_120MHz/"
+        )
+        getdist_samples, ww, ll, best_fit, covariance_matrix = load_samples(
+            folder + "chain.txt",
+            2000,
+            label_names=[
+                r"A\;[{\rm K}]",
+                r"\nu_0\;[{\rm MHz}]",
+                r"w\;[{\rm MHz}]",
+                r"\tau",
+                r"\chi",
+                r"T_{100}\;[{\rm K}]",
+                r"\beta",
+                r"\gamma",
+                r"\delta",
+                r"\epsilon",
+            ],
+        )
+        triangle_plot(
+            getdist_samples, folder + "result.pdf", legend_FS=10, label_FS=13, axes_FS=7
+        )
+
+        v, t, w, sigma, inv_sigma, det_sigma = dm.real_data(10, 60, 120)
+
+        model = dm.full_model(
+            best_fit,
+            v,
+            100,
+            model_type_signal="exp",
+            model_type_foreground="exp",
+            n_21=5,
+            n_fgpar=5,
+        )
+    elif fig == 2:
+        # Data used:  60-67, 103-119.5
+        folder = (
+            config["edges_folder"]
+            + "mid_band/polychord/20190508/case1_nominal/foreground_model_exp_gap/"
+        )
+        getdist_samples, ww, ll, best_fit, covariance_matrix = load_samples(
+            folder + "chain.txt",
+            0,
+            label_names=[
+                r"T_{100}\;[{\rm K}]",
+                r"\beta",
+                r"\gamma",
+                r"\delta",
+                r"\epsilon",
+            ],
+        )
+        triangle_plot(
+            getdist_samples, folder + "result.pdf", legend_FS=10, label_FS=10, axes_FS=5
+        )
+
+        v, t, w, sigma, inv_sigma, det_sigma = dm.real_data(10, 60, 119.5)
+
+        model = dm.full_model(
+            best_fit,
+            v,
+            100,
+            model_type_signal="exp",
+            model_type_foreground="exp",
+            n_21=0,
+            n_fgpar=5,
+        )
+    elif fig == 3:
+        # Data used:  60-65, 103-119.5
+        folder = (
+            config["edges_folder"]
+            + "mid_band/polychord/20190508/case1_nominal/foreground_model_exp_gap2/"
+        )
+        getdist_samples, ww, ll, best_fit, covariance_matrix = load_samples(
+            folder + "chain.txt",
+            0,
+            label_names=[
+                r"T_{100}\;[{\rm K}]",
+                r"\beta",
+                r"\gamma",
+                r"\delta",
+                r"\epsilon",
+            ],
+        )
+        triangle_plot(
+            getdist_samples, folder + "result.pdf", legend_FS=10, label_FS=10, axes_FS=5
+        )
+
+        v, t, w, sigma, inv_sigma, det_sigma = dm.real_data(10, 60, 119.5)
+
+        model = dm.full_model(
+            best_fit,
+            v,
+            100,
+            model_type_signal="exp",
+            model_type_foreground="exp",
+            n_21=0,
+            n_fgpar=5,
+        )
+    elif fig == 4:
+        # Data used:  60-65, 95-119.5
+        folder = (
+            config["edges_folder"]
+            + "mid_band/polychord/20190508/case1_nominal/foreground_model_exp_gap3/"
+        )
+        getdist_samples, ww, ll, best_fit, covariance_matrix = load_samples(
+            folder + "chain.txt",
+            0,
+            label_names=[
+                r"T_{100}\;[{\rm K}]",
+                r"\beta",
+                r"\gamma",
+                r"\delta",
+                r"\epsilon",
+            ],
+        )
+        triangle_plot(
+            getdist_samples, folder + "result.pdf", legend_FS=10, abel_FS=10, axes_FS=5
+        )
+
+        v, t, w, sigma, inv_sigma, det_sigma = dm.real_data(10, 60, 119.5)
+
+        model = dm.full_model(
+            best_fit,
+            v,
+            100,
+            model_type_signal="exp",
+            model_type_foreground="exp",
+            n_21=0,
+            n_fgpar=5,
+        )
+    elif fig == 5:
+        # Data used:  60-65, 95-115
+        folder = (
+            config["edges_folder"]
+            + "mid_band/polychord/20190508/case1_nominal/foreground_model_exp_gap4/"
+        )
+        getdist_samples, ww, ll, best_fit, covariance_matrix = load_samples(
+            folder + "chain.txt",
+            0,
+            label_names=[
+                r"T_{100}\;[{\rm K}]",
+                r"\beta",
+                r"\gamma",
+                r"\delta",
+                r"\epsilon",
+            ],
+        )
+        triangle_plot(
+            getdist_samples, folder + "result.pdf", legend_FS=10, label_FS=10, axes_FS=5
+        )
+
+        v, t, w, sigma, inv_sigma, det_sigma = dm.real_data(10, 60, 115)
+
+        model = dm.full_model(
+            best_fit,
+            v,
+            100,
+            model_type_signal="exp",
+            model_type_foreground="exp",
+            n_21=0,
+            n_fgpar=5,
+        )
+    elif fig == 6:
+        # Data used:  60-65, 100-115
+        folder = (
+            config["edges_folder"]
+            + "mid_band/polychord/20190508/case1_nominal/foreground_model_exp_gap5/"
+        )
+        getdist_samples, ww, ll, best_fit, covariance_matrix = load_samples(
+            folder + "chain.txt",
+            0,
+            label_names=[
+                r"T_{100}\;[{\rm K}]",
+                r"\beta",
+                r"\gamma",
+                r"\delta",
+                r"\epsilon",
+            ],
+        )
+        triangle_plot(
+            getdist_samples, folder + "result.pdf", legend_FS=10, label_FS=10, axes_FS=5
+        )
+
+        v, t, w, sigma, inv_sigma, det_sigma = dm.real_data(10, 60, 115)
+
+        model = dm.full_model(
+            best_fit,
+            v,
+            100,
+            model_type_signal="exp",
+            model_type_foreground="exp",
+            n_21=0,
+            n_fgpar=5,
+        )
+    elif fig == 7:
+        # Data used:  60-65, 97-115
+        folder = (
+            config["edges_folder"]
+            + "mid_band/polychord/20190508/case1_nominal/foreground_model_exp_gap6/"
+        )
+        getdist_samples, ww, ll, best_fit, covariance_matrix = load_samples(
+            folder + "chain.txt",
+            0,
+            label_names=[
+                r"T_{100}\;[{\rm K}]",
+                r"\beta",
+                r"\gamma",
+                r"\delta",
+                r"\epsilon",
+            ],
+        )
+        triangle_plot(
+            getdist_samples, folder + "result.pdf", legend_FS=10, label_FS=10, axes_FS=5
+        )
+
+        v, t, w, sigma, inv_sigma, det_sigma = dm.real_data(10, 60, 115)
+
+        model = dm.full_model(
+            best_fit,
+            v,
+            100,
+            model_type_signal="exp",
+            model_type_foreground="exp",
+            n_21=0,
+            n_fgpar=5,
+        )
+    elif fig == 8:
+        # Data used:  60-65, 100-115, CASE 2, cterms7, wterms8
+        folder = (
+            config["edges_folder"]
+            + "mid_band/polychord/20190508/case1_nominal/foreground_model_exp_gap7/"
+        )
+        getdist_samples, ww, ll, best_fit, covariance_matrix = load_samples(
+            folder + "chain.txt",
+            0,
+            label_names=[
+                r"T_{100}\;[{\rm K}]",
+                r"\beta",
+                r"\gamma",
+                r"\delta",
+                r"\epsilon",
+            ],
+        )
+        triangle_plot(
+            getdist_samples, folder + "result.pdf", legend_FS=10, label_FS=10, axes_FS=5
+        )
+
+        v, t, w, sigma, inv_sigma, det_sigma = dm.real_data(2, 60, 115)
+
+        model = dm.full_model(
+            best_fit,
+            v,
+            100,
+            model_type_signal="exp",
+            model_type_foreground="exp",
+            n_21=0,
+            n_fgpar=5,
+        )
+    elif fig == 9:
+        # Data used:  60-115
+        folder = (
+            config["edges_folder"]
+            + "mid_band/polychord/20190508/case1_nominal/foreground_model_exp_signal_model_tanh/"
+        )
+        getdist_samples, ww, ll, best_fit, covariance_matrix = load_samples(
+            folder + "chain.txt",
+            0,
+            label_names=[
+                r"A\;[{\rm K}]",
+                r"\nu_0\;[{\rm MHz}]",
+                r"w\;[{\rm MHz}]",
+                r"\tau_1",
+                r"\tau_2",
+                r"T_{100}\;[{\rm K}]",
+                r"\beta",
+                r"\gamma",
+                r"\delta",
+                r"\epsilon",
+            ],
+        )
+        triangle_plot(
+            getdist_samples, folder + "result.pdf", legend_FS=10, label_FS=10, axes_FS=5
+        )
+
+        v, t, w, sigma, inv_sigma, det_sigma = dm.real_data(10, 60, 115)
+
+        model = dm.full_model(
+            best_fit,
+            v,
+            100,
+            model_type_signal="tanh",
+            model_type_foreground="exp",
+            n_21=5,
+            n_fgpar=5,
+        )
+    return v, t, w, model
 
 
 def plot_foreground_analysis():
