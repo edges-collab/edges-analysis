1d0eee068204c471716d1b0db627990db1e7a9ff
nmahesh1412 <nivedita.mahesh@asu.edu>
2021-10-04 12:45:57 -0700

    basic test for max_fm

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -65,3 +65,8 @@ def test_negpower_filter(cal_step):
 def test_150mhz_filter(cal_step):
     out_flags = filters.filter_150mhz(data=cal_step, threshold=1)
     assert len(out_flags) == 2
+
+
+def test_max_fm_filter(cal_step):
+    out_flags = filters.maxfm_filter(data=cal_step, threshold=200)
+    assert len(out_flags) == 2
