3ceeda46124b044bb955181d23270628df098871
nmahesh1412 <37714229+nmahesh1412@users.noreply.github.com>
2021-10-22 09:58:36 -0700

    Update filters.py
    fix small bug

--- src/edges_analysis/analysis/filters.py
+++ src/edges_analysis/analysis/filters.py
@@ -909,19 +909,19 @@ def _rfi_filter_factory(method: str):
         mask = (data.raw_frequencies >= freq_range[0]) & (
             data.raw_frequencies <= freq_range[1]
         )
-        if data.spectrum.ndim == 2:
-            out_flags = tools.run_xrfi(
-                method=method,
-                spectrum=data.spectrum[..., mask],
-                freq=data.raw_frequencies[mask],
-                flags=flags[..., mask],
-                weights=data.weights[..., mask],
-                n_threads=n_threads,
-                **kwargs,
-            )
+        
+        out_flags = tools.run_xrfi(
+            method=method,
+            spectrum=data.spectrum[..., mask],
+            freq=data.raw_frequencies[mask],
+            flags=flags[..., mask],
+            weights=data.weights[..., mask],
+            n_threads=n_threads,
+            **kwargs,
+        )
 
-            out = np.zeros_like(flags)
-            out[..., mask] = out_flags
+        out = np.zeros_like(flags)
+        out[..., mask] = out_flags
 
         return out
 
