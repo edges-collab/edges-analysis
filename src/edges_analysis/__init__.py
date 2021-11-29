f9d0ddad884979d80073b784725d64684eac043f
Steven Murray <steven.murray@curtin.edu.au>
2021-11-29 11:22:20 -0700

    chore: don't cover DistributionNotFound

--- src/edges_analysis/__init__.py
+++ src/edges_analysis/__init__.py
@@ -3,7 +3,7 @@ from pkg_resources import DistributionNotFound, get_distribution
 
 try:
     __version__ = get_distribution(__name__).version
-except DistributionNotFound:
+except DistributionNotFound:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del get_distribution, DistributionNotFound
