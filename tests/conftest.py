8102edd61985a684269cd9a6e3852f58fad20b19
nmahesh1412 <nivedita.mahesh@asu.edu>
2021-12-08 18:49:06 -0700

    actually invoked the calibrate_settings2 definition

--- tests/conftest.py
+++ tests/conftest.py
@@ -109,7 +109,7 @@ def calibrate_settings2(integration_test_data: Path) -> Path:
     settings = {
         "calfile": str(integration_test_data / "calfile_Rcv_2017_05.h5"),
         "s11_path": str(
-            integration_test_data / "S11_blade_low_band_2015_342_03_14.txt.csv "
+            integration_test_data / "S11_blade_low_band_2015_342_03_14.txt.csv"
         ),
         "balun_correction": True,
         "antenna_correction": False,
@@ -179,6 +179,31 @@ def cal_step(
     return tuple(
         read_step(fl)
         for fl in sorted((raw_step[0].filename.parent / "calibrated").glob("*.h5"))
+    )
+
+
+@pytest.fixture(scope="session")
+def cal_step2(
+    raw_step: Tuple[RawData, RawData],
+    calibrate_settings2: Path,
+    edges_config: dict,
+    settings: Path,
+) -> Tuple[CalibratedData, CalibratedData]:
+    invoke(
+        cli.process,
+        [
+            "calibrate",
+            str(calibrate_settings2),
+            "-i",
+            str(raw_step[0].filename.parent / "*.h5"),
+            "-l",
+            "calibrated2",
+        ],
+    )
+
+    return tuple(
+        read_step(fl)
+        for fl in sorted((raw_step[0].filename.parent / "calibrated2").glob("*.h5"))
     )
 
 
