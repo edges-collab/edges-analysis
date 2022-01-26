19c95f66b87993e19e5f4d55bb7ae12c0ed41d3c
Akshatha Vydula <vydula@enterprise>
2022-01-26 13:28:19 -0700

    Added outlines to all the sky maps

--- src/edges_analysis/analysis/sky_models.py
+++ src/edges_analysis/analysis/sky_models.py
@@ -102,7 +102,7 @@ class SkyModel:
     """
     A base class representing a sky model at one frequency in healpix.
 
-    Do not use this class -- use a specific subclass that specifies an actual sky model.
+    Do not use this class -- use a specific subclass that specifies an actual sky model
 
     Each sky model's data is gotten from lambda, and the first time it is accessed it
     is downloaded and cached on disk using astropy.
@@ -216,6 +216,16 @@ class Haslam408(SkyModel):
         "https://lambda.gsfc.nasa.gov/data/foregrounds/haslam/"
         "lambda_haslam408_dsds.fits"
     )
+    """
+    408MHz radio continuum all-sky map of Haslam etal combines data from
+    4 different surveys. The destriped/desourced (dsds) version was constructed from
+    survey data obtained from the archives of the NCSA ADIL in
+    galactic coordinates. The data were then transformed to a celestial coordinate
+    grid and subsequently processed further in
+    both the Fourier and spatial domains to mitigate baseline striping and strong
+    point sources. These data were then
+    interpolated onto a HEALPix projection.
+    """
     frequency = 408.0
     header_hdu = 1
 
@@ -225,6 +235,20 @@ class Remazeilles408(SkyModel):
         "https://lambda.gsfc.nasa.gov/data/foregrounds/haslam_2014/haslam408_dsds_"
         "Remazeilles2014.fits"
     )
+    """
+    Remazeilles et al. 2014 have re-evaluated and re-processed the rawest Haslam
+    408 MHz data, available from the Max Planck
+    Institute for Radioastronomy Survey Sampler website, to produce an improved
+    source-subtracted and destriped 408 MHz all
+    sky map. Large-scale striations associated with correlated low-frequency noise in
+    the scan direction are reduced using a Fourier-based filtering technique.
+    The most important improvement results from the removal of extra-galactic sources.
+    An iterative combination of two techniques − two-dimensional Gaussian fitting and
+    minimum curvature spline surface inpainting− are used to remove the brightest
+    sources (with flux density larger than 2 Jy). Four products are made publicly
+    available. They are described below. The sky maps are generated using the HEALPix
+    sky pixelization scheme.
+    """
     frequency = 408.0
     header_hdu = 1
 
@@ -234,12 +258,34 @@ class LW150(SkyModel):
         "https://lambda.gsfc.nasa.gov/data/foregrounds/landecker_150/"
         "lambda_landecker_wielebinski_150MHz_SARAS_recalibrated_hpx_r8.fits"
     )
+    """
+    Patra et al. 2015, provided a recalibration of the 150 MHz map based on comparison
+    with absolutely calibrated sky brightness measurements between 110 and 175 MHz made
+    with the SARAS spectrometer, and Monsalve et al. 2021, provided a recalibration
+    based on comparison with absolutely calibrated measurements from the EDGES.
+    The correction factors that were obtained are
+    SARAS: scale 1.048 +- 0.008, offset -22.4 +- 8.0 K
+    EDGES: scale 1.112 +- 0.023, offset 0.7 +- 6.0 K
+    Monsalve et al. note that the differences may be because EDGES is a southern
+    hemisphere instrument, SARAS is a northern hemisphere instrument, and the all-sky
+    150 MHz map was constructed from separate southern, northern, and intermediate
+    declination surveys
+    """
     header_hdu = 1
     frequency = 150.0
 
 
 class Guzman45(SkyModel):
     url = "https://lambda.gsfc.nasa.gov/data/foregrounds/maipu_45/MAIPU_MU_1_64.fits"
+    """
+    Guzmán et al. (2011) produced an all-sky map of 45 MHz emission by combining data
+    from the 45 MHz survey of Alvarez et al (1997) between declinations -90° and +19.1°
+    with data from the 46.5 MHz survey of Maeda et al. (1999) between declinations
+    +5° and +65°. The southern survey was made with the Maipu Radio Astronomy
+    Observatory 45-MHz array with a beam of 4.6° (RA)x 2.4° (dec) FWHM.
+    The northern survey was made with the Japanese Middle and Upper atmosphere
+    (MU) radar array with a beam of 3.6° x 3.6°.
+    """
     frequency = 45.0
     header_hdu = 1
     data_name = "UNKNOWN1"
@@ -250,20 +296,57 @@ class Guzman45(SkyModel):
 
         return sky_map
 
+
 class WhamHAlpha(SkyModel):
     url = "https://lambda.gsfc.nasa.gov/data/foregrounds/wham/lambda_WHAM_1_256.fits"
+    """
+    The Wisconsin H-Alpha Mapper (WHAM) made a survey of H alpha intensity over the full
+    sky with a 1 degree beam and velocity resolution of 12 km/s. The WHAM public data
+    release DR1 includes a data product containing H alpha intensity integrated
+    between -80 and +80 km/s LSR velocity for each observed sky position.
+    The Centre d'Analyse de Données Etendues group used these data to form an all-sky
+    HEALPix format map following method described in Appendix A of Paradis etal 2012
+    Their HEALPix map is provided here. LAMBDA made a new version of their FITS file
+    with minor changes to the headers to make them compliant with FITS standards.
+
+    """
     frequency = 457108000
     header_hdu = 1
     data_name = "UNKNOWN1"
 
+
 class PlanckCO(SkyModel):
-    url = "https://irsa.ipac.caltech.edu/data/Planck/release_2/all-sky-maps/maps/component-maps/foregrounds/COM_CompMap_CO-commander_0256_R2.00.fits"
+    url = (
+        "https://irsa.ipac.caltech.edu/data/Planck/release_2/all-sky-maps/maps"
+        "/component-maps/foregrounds/COM_CompMap_CO-commander_0256_R2.00.fits"
+    )
+    """
+    Planck All Sky Maps are in HEALPix format, with Nside 2048, in Galactic coordinates
+    and nested ordering. The signal is given in units of Kcmb for 30-353 GHz.
+    Unpolarized maps contain 3 planes, labeled I_Stokes (intensity), Hits (hit count),
+    and II_cov (variance of I_Stokes). Polarized maps contain 10 planes, labeled
+    I_Stokes (intensity), Q_Stokes and U_Stokes (linear polarization), Hits (hit
+    count), II_cov, QQ_cov, and UU_cov (variance of each of the Stokes parameters),
+    and IQ_cov, IU_cov, and QU_cov (covariances between the Stokes parameters).
+    """
     frequency = 115270
     header_hdu = 1
-    data_name = "I_MEAN" 
+    data_name = "I_MEAN"
+
 
 class HI4PI(SkyModel):
-    url = "https://lambda.gsfc.nasa.gov/data/foregrounds/ebv_2017/mom0_-90_90_1024.hpx.fits"
+    url = (
+        "https://lambda.gsfc.nasa.gov/data/foregrounds/ebv_2017/mom0_-90_90_1024"
+        ".hpx.fits"
+    )
+    """
+    The HI 4-PI Survey (HI4PI) is a 21-cm all-sky survey of neutral atomic hydrogen.
+    It is constructed from the Effelsberg Bonn HI Survey (EBHIS), made with the 100-m
+    radio telescope at Effelsberg/Germany, and the Galactic All-Sky Survey (GASS),
+    observed with the Parkes 64-m dish in Australia. HI4PI comprises HI line emission
+    from the Milky Way. This dataset is the atomic neutral hydrogen (HI) column density
+    map derived from HI4PI (|vLSR|< 600 km/s).
+    """
     frequency = 1420.40575177
     header_hdu = 1
-    data_name = "NHI" 
+    data_name = "NHI"
