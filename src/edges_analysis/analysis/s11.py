48743334c34a42cf0d130b89e084f8dd5dff3bb4
Steven Murray <steven.g.murray@asu.edu>
2022-03-16 12:04:38 -0700

    fix: don't pass check to init

--- src/edges_analysis/analysis/s11.py
+++ src/edges_analysis/analysis/s11.py
@@ -37,14 +37,9 @@ class AntennaS11(LoadS11):
         files = sorted(files)
         assert len(files) == 4
         standards = StandardsReadings(
-            *[
-                VNAReading.from_s1p(fl, f_low=f_low, f_high=f_high, check=False)
-                for fl in files[:3]
-            ]
+            *[VNAReading.from_s1p(fl, f_low=f_low, f_high=f_high) for fl in files[:3]]
         )
-        external = VNAReading.from_s1p(
-            files[-1], f_low=f_low, f_high=f_high, check=False
-        )
+        external = VNAReading.from_s1p(files[-1], f_low=f_low, f_high=f_high)
 
         # Note that here the
         load_s11 = (
