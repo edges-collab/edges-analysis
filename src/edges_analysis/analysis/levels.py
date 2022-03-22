fix: ensure we use gha-averaged quantities

src/edges_analysis/analysis/levels.py
@@ -2360,15 +2360,12 @@ class CombinedBinnedData(_ModelMixin, _ReductionStep, _CombinedFileMixin):
                 , got {gha_bin_size}"
             )
 
-        flags = prev_step.get_flags()
-
         # Bin in GHA using the models and residuals
         params, resids, weights, gha_edges = cls.bin_gha(
             prev_step,
             gha_bin_size,
             gha_min,
             gha_max,
-            flags=flags,
         )
 
         freq = FrequencyRange(
@@ -2399,9 +2396,9 @@ class CombinedBinnedData(_ModelMixin, _ReductionStep, _CombinedFileMixin):
             logger.info(f".... produced {len(f)} frequency bins.")
         else:
             f = freq.freq
-            params = prev_step.model_params
-            new_wght = wght
-            new_resid = resid
+            params = np.array(params)
+            new_wght = np.array(wght)
+            new_resid = np.array(resid)
 
         data = {"weights": np.array(new_wght), "resids": np.array(new_resid)}
 
@@ -2438,7 +2435,6 @@ class CombinedBinnedData(_ModelMixin, _ReductionStep, _CombinedFileMixin):
         gha_bin_size: float,
         gha_min: float | None,
         gha_max: float | None,
-        flags=None,
     ):
         """Bin a list of files into small aligning bins of GHA."""
         if gha_min is None and gha_max is None:
@@ -2473,7 +2469,6 @@ class CombinedBinnedData(_ModelMixin, _ReductionStep, _CombinedFileMixin):
         for i, (p, r, w) in enumerate(
             zip(combined_obj.model_params, combined_obj.resids, combined_obj.weights)
         ):
-
             params[i], resids[i], weights[i] = averaging.bin_gha_unbiased_regular(
                 p, r, w, gha, gha_edges
             )