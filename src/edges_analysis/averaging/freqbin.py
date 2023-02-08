568dcb7f2e64572862257e0567c5cccb06564050
Steven Murray <steven.g.murray@asu.edu>
2023-02-08 16:07:09 -0700

    feat: add maintain flags feature

--- src/edges_analysis/averaging/freqbin.py
+++ src/edges_analysis/averaging/freqbin.py
@@ -80,32 +80,72 @@ def freq_bin_with_models(
 
 
 @gsregister("reduce")
-def gauss_smooth(data: GSData, size: int, decimate_at: int | None = None) -> np.ndarray:
-    """Smooth x with a Gaussian function, and reduces the size of the array."""
+def gauss_smooth(
+    data: GSData,
+    size: int,
+    decimate: bool = True,
+    decimate_at: int = None,
+    flag_threshold: float = 0,
+    maintain_flags: bool = False,
+) -> np.ndarray:
+    """Smooth data with a Gaussian function, and reduce the size of the array.
+
+    Parameters
+    ----------
+    data
+        The :class:`GSData` object to smooth.
+    size
+        The size of the Gaussian smoothing kernel. The ultimate size of the kernel will
+        be ``4*size``. The final array will be decimated by a factor of ``size``.
+    decimate
+        Whether to decimate the array by a factor of ``size``.
+    decimate_at
+        The index at which to start decimating the array. If ``None``, this will be
+        ``size//2``.
+    flag_threshold
+        The threshold of flagged samples to flag a channel. Set to 0.25 to flag in the
+        same way as Alan's C-Code. In detail, for a dataset with uniform weights (but
+        potentially some flagged bins), any smoothed channel whose integrated window
+        *not* counting flagged channels is smaller than ``size*flag_threshold`` will be
+        flagged.
+    maintain_flags
+        Whether to maintain the flags in the data. If ``True``, any fine-channels
+        that were originally flagged will be flagged in the output. The default
+        behaviour is to simply sum all weights within the window, but Alan's code
+        uses this feature.
+    """
     assert isinstance(size, int)
+    if decimate:
 
-    if decimate_at is None:
-        decimate_at = size // 2
+        if decimate_at is None:
+            decimate_at = size // 2
 
-    assert isinstance(decimate_at, int)
-    assert decimate_at < size
-
+        assert isinstance(decimate_at, int)
+        assert decimate_at < size
+    else:
+        decimate_at = 0
     # This choice of size scaling corresponds to Alan's C code.
     y = np.arange(-size * 4, size * 4 + 1) * 2 / size
     window = np.exp(-(y**2) * 0.69)
-
+    if not decimate:
+        decimate = 1
+    else:
+        decimate = size
     sums = convolve1d(data.flagged_nsamples * data.data, window, mode="nearest")[
-        ..., decimate_at::size
+        ..., decimate_at::decimate
     ]
-    nsamples = convolve1d(data.flagged_nsamples, window, mode="nearest")[
-        ..., decimate_at::size
-    ]
+    nsamples = convolve1d(data.flagged_nsamples, window, mode="nearest")
+    if maintain_flags:
+        nsamples[data.complete_flags] = 0
 
+    nsamples = nsamples[..., decimate_at::decimate]
+
+    nsamples[nsamples / data.nsamples.max() <= size * flag_threshold] = 0
     return data.update(
         data=sums / nsamples,
         nsamples=nsamples,
         flags={},
-        freq_array=data.freq_array[decimate_at::size],
+        freq_array=data.freq_array[decimate_at::decimate],
         data_model=None,
     )
 
