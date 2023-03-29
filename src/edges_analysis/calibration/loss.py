0708ee846395c2cf51d7fd7b1623932c2ffabc5a
nmahesh1412 <nivedita.mahesh@asu.edu>
2023-03-29 15:42:50 -0700

    setting connector length defaults to the ones in Alan's code and ones that are confirmed with measurements in Memo 181, 210

--- src/edges_analysis/calibration/loss.py
+++ src/edges_analysis/calibration/loss.py
@@ -56,7 +56,7 @@ def balun_and_connector_loss(
 
     # Metal conductivity
     sigma_copper = 1 * sigma_copper0
-    sigma_brass = 0.24 * sigma_copper0
+    sigma_brass =  0.29* sigma_copper0 #0.24
 
     sigma_xx_inner = 0.24 * sigma_copper0
     sigma_xx_outer = 0.024 * sigma_copper0
@@ -75,7 +75,7 @@ def balun_and_connector_loss(
     parameters = {
         "low": {
             "balun_length": 43.6 * inch2m,
-            "connector_length": 0.8 * inch2m,
+            "connector_length":  1.18* inch2m, #0.8
             "er_air": 1.07,
             "ric_b": ((5 / 16) * inch2m) / 2,
             "roc_b": ((3 / 4) * inch2m) / 2,
