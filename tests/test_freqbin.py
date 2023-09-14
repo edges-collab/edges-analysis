745d70b155b75c56f062c7df1baf887f299e7621
Steven Murray <steven.murray@curtin.edu.au>
2023-09-14 14:59:48 -0700

    test: fix freqbin tests

--- tests/test_freqbin.py
+++ tests/test_freqbin.py
@@ -4,9 +4,9 @@ from edges_analysis.averaging import freqbin
 from edges_analysis.gsdata import GSFlag
 
 
-def test_freq_bin_direct(cal_step):
-    new = freqbin.freq_bin(cal_step[0], resolution=2, debias=False)
-    assert len(new.freq_array) == len(cal_step[0].freq_array) // 2
+def test_freq_bin_direct(gsd_ones):
+    new = freqbin.freq_bin(gsd_ones, resolution=2, debias=False)
+    assert len(new.freq_array) == len(gsd_ones.freq_array) // 2
 
 
 def test_freqbin_gaussian(gsd_ones):
@@ -26,4 +26,4 @@ def test_freqbin_gaussian(gsd_ones):
     data2 = data.update(flags={"f0": GSFlag(flags, axes=("freq",))})
     new = freqbin.gauss_smooth(data2, size=2, maintain_flags=True, decimate=False)
 
-    assert np.all(new.nsamples[:, :, :, 0] == 0)
+    assert np.all(new.complete_flags[:, :, :, 0] == 0)
