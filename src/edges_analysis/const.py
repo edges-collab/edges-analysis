70aad519ca4abd2d5f05bf6bdefa5f3121fcadf8
Steven Murray <steven.g.murray@asu.edu>
2024-03-18 16:41:13 +0100

    feat: add edges3 scope

--- src/edges_analysis/const.py
+++ src/edges_analysis/const.py
@@ -20,6 +20,15 @@ KNOWN_TELESCOPES["edges3-devon"] = Telescope(
     x_orientation=0.0 * apu.deg,
 )
 
+KNOWN_TELESCOPES["edges3"] = Telescope(
+    name="edges3",
+    location=apc.EarthLocation(lat=-26.7 * apu.deg, lon=116.5 * apu.deg),
+    pols=("xx",),
+    integration_time=13.0 * apu.s,
+    x_orientation=0.0 * apu.deg,
+)
+
+
 galactic_centre_lst = 17 + (45 / 60) + (40.04 / (60 * 60))
 absolute_zero = (0 * apu.deg_C).to(apu.K, equivalencies=apu.temperature()).value
 edges_location = KNOWN_TELESCOPES["edges-low"].location
