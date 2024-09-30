fix: average_over_times

src/edges_analysis/averaging/combiners.py
@@ -9,6 +9,7 @@ from typing import Literal
 import numpy as np
 from astropy import units as un
 from astropy.coordinates import Longitude
+from astropy.time import Time
 from pygsdata import GSData, gsregister
 
 logger = logging.getLogger(__name__)
@@ -95,12 +96,19 @@ def average_over_times(
         data=new_data[:, :, None, :],
         residuals=mean_resids[:, :, None, :] if use_resids else None,
         times=np.atleast_2d(np.mean(data.times)),
-        time_ranges=np.array([[[data.time_ranges.min(), data.time_ranges.max()]]]),
+        time_ranges=Time(
+            np.array([[[data.time_ranges.min().jd, data.time_ranges.max().jd]]]),
+            format="jd",
+        ),
+        lsts=Longitude(np.array([[np.mean(data.lsts.hour)]]) * un.hour),
         lst_ranges=Longitude(
             np.array([[[data.lst_ranges.hour.min(), data.lst_ranges.hour.max()]]])
             * un.hour
         ),
-        nsamples=ntot,
+        effective_integration_time=np.mean(data.effective_integration_time, axis=2)[
+            :, :, None
+        ],
+        nsamples=ntot[:, :, None],
         flags={},
     )
 

tests/test_averaging.py
@@ -4,8 +4,9 @@ from __future__ import annotations
 
 import numpy as np
 import pytest
-from edges_analysis.averaging import averaging
+from edges_analysis.averaging import averaging, combiners
 from edges_cal import modelling as mdl
+from pygsdata import GSData
 from pytest_cases import fixture_ref as fxref
 from pytest_cases import parametrize
 
@@ -215,3 +216,8 @@ class TestBinArray:
         )
         assert np.all(mean == 1)
         assert np.all(wght == 4)
+
+
+def test_averaging_over_times(gsd_ones: GSData):
+    new = combiners.average_over_times(gsd_ones)
+    assert np.all(new.data == 1.0)