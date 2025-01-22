fix: average_over_times setting of new mean time and lsts

src/edges_analysis/averaging/lstbin.py
@@ -67,6 +67,7 @@ def get_lst_bins(
 def average_over_times(
     data: GSData,
     nsamples_strategy: NsamplesStrategy = NsamplesStrategy.FLAGGED_NSAMPLES,
+    reference_lst: Longitude = Longitude(12 * un.hour),
     use_resids: bool | None = None,
     fill_value: float = 0.0,
 ) -> GSData:
@@ -79,6 +80,9 @@ def average_over_times(
     nsamples_strategy
         The strategy to use when defining the weights of each sample. See
         :class:`~edges_analysis.averaging.NsamplesStrategy` for more information.
+    reference_lst
+        An LST set as the central LST when finding the new mean LST. All LSTs will
+        be wrapped within 12 hours of this reference before taking the mean.
     use_resids : bool, optional
         Whether to average the residuals and add them back to the mean model, or simply
         average the data directly.
@@ -107,28 +111,42 @@ def average_over_times(
 
     new_data[np.isnan(new_data)] = fill_value
 
+    # The new time will be the mean unflagged time
+    ww = np.any(w > 0, axis=(0, 1, 3))
+    times = Time(np.atleast_2d(np.mean(data.times.jd[ww], axis=0)), format="jd")
+    time_ranges = Time(
+        np.array([
+            [
+                data.time_ranges.jd[ww].min(axis=(0, 2)),
+                data.time_ranges.jd[ww].max(axis=(0, 2)),
+            ]
+        ]).transpose((0, 2, 1)),
+        format="jd",
+    )
+
+    # To get the best LST representing the data use the reference
+    lsts = data.lsts.hour.copy()
+    lsts[lsts <= reference_lst.hour - 12] += 24
+    lsts[lsts > reference_lst.hour + 12] -= 24
+
+    lst_ranges = data.lst_ranges.copy()
+    lst_ranges[lst_ranges <= reference_lst - 12 * un.hourangle] += 24 * un.hourangle
+    lst_ranges[lst_ranges > reference_lst + 12 * un.hourangle] -= 24 * un.hourangle
+
     return data.update(
         data=new_data[:, :, None, :],
         residuals=mean_resids[:, :, None, :] if use_resids else None,
-        times=np.atleast_2d(np.mean(data.times, axis=0)),
-        time_ranges=Time(
-            np.array([
-                [
-                    data.time_ranges.jd.min(axis=(0, 2)),
-                    data.time_ranges.jd.max(axis=(0, 2)),
-                ]
-            ]).transpose((0, 2, 1)),
-            format="jd",
-        ),
-        lsts=Longitude(np.atleast_2d(np.mean(data.lsts.hour, axis=0)) * un.hour),
+        times=times,
+        time_ranges=time_ranges,
+        lsts=Longitude(np.atleast_2d(np.mean(lsts[ww], axis=0)) * un.hourangle),
         lst_ranges=Longitude(
             np.array([
                 [
-                    data.lst_ranges.hour.min(axis=(0, 2)),
-                    data.lst_ranges.hour.max(axis=(0, 2)),
+                    lst_ranges[ww, :, 0].min(axis=0).hour,
+                    lst_ranges[ww, :, 1].max(axis=0).hour,
                 ]
             ]).transpose((0, 2, 1))
-            * un.hour
+            * un.hourangle
         ),
         effective_integration_time=np.mean(data.effective_integration_time, axis=2)[
             :, :, None