b559e13288d902fcfdb02d55e5254c984a28de06
Steven Murray <steven.g.murray@asu.edu>
2026-01-06 08:45:55 +1030

    test: fix pooch data tests

--- tests/test_pooch_data.py
+++ tests/test_pooch_data.py
@@ -2,12 +2,14 @@ from edges.data.cli import app
 
 
 def test_fetch_b18_testing(capsys):
-    app("fetch_b18", dataset="testing")
-    assert "/S11" in capsys.readouterr().out
-    assert "/Resistance" in capsys.readouterr().out
-    assert "Fetched B18 calibration testing data files:" in capsys.readouterr().out
+    app("data fetch_b18 --dataset testing", result_action="return_value")
+    out = capsys.readouterr().out
+    assert "/S11" in out
+    assert "/Resistance" in out
+    assert "Fetched B18 calibration testing data files:" in out
 
 
 def test_fetch_b18_none(capsys):
-    app("fetch_b18", dataset="none")
-    assert "No files fetched." in capsys.readouterr().out
+    app("data fetch_b18 --dataset none", result_action="return_value")
+    out = capsys.readouterr().out
+    assert "No files fetched." in out
